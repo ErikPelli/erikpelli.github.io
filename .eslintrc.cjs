aceeead7f4043c23d65f6e63cd4ed48a51542a70
Erik Pellizzon <erikpellizzon@gmail.com>
2023-08-13 00:29:36 +0200

    Initialize sveltekit project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: "svelte-eslint-parser",
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		}
+	],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+const config = {
+	content: [
+		"./src/**/*.{html,js,svelte,ts,md}",
+	],
+	darkMode: 'class'
+};
+
+module.exports = config;
