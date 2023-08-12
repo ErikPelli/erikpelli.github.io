79d369a572f14237b9d26ff5d06e42b65dd23dc9
Erik Pellizzon <erikpellizzon@gmail.com>
2023-08-13 00:30:52 +0200

    Add home page made with tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script>
+	import "../app.postcss";
+
+	// Social
+	import GithubIcon from "svelte-feather-icons/src/icons/GithubIcon.svelte";
+	import LinkedinIcon from "svelte-feather-icons/src/icons/LinkedinIcon.svelte";
+	import TelegramIcon from "svelte-feather-icons/src/icons/SendIcon.svelte";
+
+	const actualYear = new Date().getFullYear();
+</script>
+
+<div class="page">
+	<header class="w-full bg-gray-800 p-6">
+		<div class="flex flex-col md:flex-row">
+			<div class="flex flex-col md:flex-row space-x-6 text-left md:text-center">
+				<div class="flex flex-row text-gray-300 text-center">
+					<span class="text-5xl">@</span>
+					<h1 class="text-8xl italic font-semibold">ErikPelli</h1>
+				</div>
+				<h2 class="text-4xl text-gray-500 uppercase font-semibold md:font-light">Portfolio Website</h2>
+			</div>
+			<div
+				class="flex flex-row md:flex-col bg-gray-600 rounded-lg p-1 mt-3 space-x-1 md:space-x-0 md:space-y-1 md:w-10 md:ml-auto justify-center">
+				<a href="https://github.com/ErikPelli" target="_blank">
+					<GithubIcon class="rounded-3xl bg-black text-white p-1" size="2x" />
+				</a>
+				<a href="https://linkedin.com/in/erikpelli" target="_blank">
+					<LinkedinIcon class="rounded-3xl bg-blue-700 text-white p-1" size="2x" />
+				</a>
+				<a href="https://telegram.me/ErikP" target="_blank">
+					<TelegramIcon class="rounded-3xl bg-white text-blue-400 p-1" size="2x" />
+				</a>
+			</div>
+		</div>
+		<div class="flex lg:justify-end w-full">
+			<nav class="flex justify-center space-x-2 text-center mt-4 my-2 w-full md:w-3/5 lg:w-96">
+				<a href="/" class="p-1 rounded-lg bg-gray-300 w-1/4 hover:w-1/3 uppercase">Home</a>
+				<a href="/about" class="p-1 rounded-lg bg-gray-300 w-1/4 hover:w-1/3 uppercase">About me</a>
+				<a href="/projects" class="p-1 rounded-lg bg-gray-300 w-1/4 hover:w-1/3 uppercase">Projects</a>
+			</nav>
+		</div>
+	</header>
+
+	<slot />
+</div>
+
+<footer class="w-full bg-gray-800 text-gray-500 text-center p-6">
+	<div class="flex items-center justify-center">
+		<hr class="w-full border-gray-700 border-2" />
+	</div>
+	<p class="text-xl p-2 pt-4">Copyright © {actualYear} - Erik Pellizzon</p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+	// Technologies
+	import SvelteLogo from "$lib/components/svelte-logo.svelte";
+	import TailwindLogo from "$lib/components/tailwind-logo.svelte";
+
+	// Companies
+	import GardaLogo from "$lib/components/garda-logo.svelte";
+</script>
+
+<div class="flex justify-center space-x-2 bg-gray-200 p-2">
+	<span class="text-gray-600 text-xl italic font-bold">Made with</span>
+	<a href="https://kit.svelte.dev/" target="_blank">
+		<SvelteLogo size="h-10" />
+	</a>
+	<a href="https://tailwindcss.com/" target="_blank">
+		<TailwindLogo size="h-10" />
+	</a>
+</div>
+
+<div class="flex flex-col justify-around h-screen my-6">
+	<div class="flex flex-col text-center h-1/3">
+		<span class="invisible text-[0px]">Garda Security</span>
+		<span class="invisible text-[0px]">Crisis24</span>
+		<span class="invisible text-[0px]">GardaWorld</span>
+		<a href="https://garda.com" target="_blank">
+			<GardaLogo size="mx-auto h-10" />
+		</a>
+		<h1 class="text-4xl font-semibold text-[#747678]">Software Engineer</h1>
+	</div>
+
+	<div class="text-center h-1/3">
+		<p class="text-gray-400 text-4xl xl:text-6xl font-extralight">Do you have any proposal?</p>
+		<p class="text-gray-300 text-3xl xl:text-5xl font-bold">Send me an
+			<a href="mailto:erikpellizzon1@gmail.com" class="text-blue-400">email</a>
+		</p>
+	</div>
+
+	<div class="flex flex-row space-x-2 flex-wrap justify-center p-4">
+		<a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40"
+					 height="40" />
+		</a>
+		<a href="https://cassandra.apache.org/" target="_blank" rel="noreferrer">
+			<img src="https://www.vectorlogo.zone/logos/apache_cassandra/apache_cassandra-icon.svg" alt="cassandra" width="40"
+					 height="40" />
+		</a>
+		<a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+					 alt="docker" width="40" height="40" />
+		</a>
+		<a href="https://cloud.google.com" target="_blank" rel="noreferrer">
+			<img src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg" alt="gcp" width="40"
+					 height="40" />
+		</a>
+		<a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+			<img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" />
+		</a>
+		<a href="https://golang.org" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" alt="go" width="40"
+					 height="40" />
+		</a>
+		<a href="https://graphql.org" target="_blank" rel="noreferrer">
+			<img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="graphql" width="40" height="40" />
+		</a>
+		<a href="https://www.java.com" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java"
+					 width="40" height="40" />
+		</a>
+		<a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+					 alt="mongodb" width="40" height="40" />
+		</a>
+		<a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+			<img
+				src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+				alt="postgresql" width="40" height="40" />
+		</a>
+		<a href="https://www.python.org" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python"
+					 width="40" height="40" />
+		</a>
+		<a href="https://rubyonrails.org" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rails/rails-original-wordmark.svg"
+					 alt="rails" width="40" height="40" />
+		</a>
+		<a href="https://redis.io" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg"
+					 alt="redis"
+					 width="40" height="40" />
+		</a>
+		<a href="https://www.ruby-lang.org/en/" target="_blank" rel="noreferrer">
+			<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/ruby/ruby-original.svg" alt="ruby"
+					 width="40"
+					 height="40" />
+		</a>
+		<a href="https://www.selenium.dev" target="_blank" rel="noreferrer">
+			<img
+				src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/selenium-logo.svg"
+				alt="selenium" width="40" height="40" />
+		</a>
+		<a href="https://spring.io/" target="_blank" rel="noreferrer">
+			<img src="https://www.vectorlogo.zone/logos/springio/springio-icon.svg" alt="spring" width="40" height="40" />
+		</a>
+	</div>
+</div>
+
